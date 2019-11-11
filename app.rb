cde9c887071e8a8ae238ba43345577102004faaf
Christian Cain <christian22cain@yahoo.com>
2019-11-11 13:51:18 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Wow (Owen Wilson voice)."
   end
 
 end
